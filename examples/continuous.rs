5d5ee0a52b1eee3a0df9792867a9c5ec5c6c34e4
Danilo Bargen <mail@dbrgn.ch>
2018-05-15 23:34:58 +0200

    Fix continuous example
    The "not ready" status is now an error, which breaks the example.

--- examples/continuous.rs
+++ examples/continuous.rs
@@ -8,6 +8,18 @@ use mcp3425::{MCP3425, Config, Resolution, Gain};
 
 fn main() {
     println!("Hello, MCP3425!");
+    println!();
+    println!("------");
+    println!();
+    println!("This example will write the config to the device.");
+    println!("It will then immediately read data from the device, which will fail");
+    println!("because no measurement was awaited.");
+    println!("Then the program will sleep for 150ms and read the measurement twice");
+    println!("in a row. The first measurement should succeed, the second one should");
+    println!("fail because the ADC is being polled too quickly.");
+    println!();
+    println!("------");
+    println!();
 
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let address = 0x68;
@@ -16,9 +28,9 @@ fn main() {
 
     println!("Writing configuration to device: {:?}", &config);
     adc.set_config(&config).unwrap();
-    println!("Reading measurement: {:?}", &adc.read_measurement().unwrap());
+    println!("Reading measurement: {:?}", &adc.read_measurement());
     println!("Sleeping 150ms");
     Delay.delay_ms(150u8);
-    println!("Reading measurement: {:?}", &adc.read_measurement().unwrap());
-    println!("Reading measurement: {:?}", &adc.read_measurement().unwrap());
+    println!("Reading measurement: {:?}", &adc.read_measurement());
+    println!("Reading measurement: {:?}", &adc.read_measurement());
 }
